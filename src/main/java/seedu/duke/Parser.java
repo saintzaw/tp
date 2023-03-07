18ad1efaffacf59866b0e99825861c757e1963da
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-03-07 22:46:28 +0800

    Finished addModule and modified input parsing

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -5,10 +5,13 @@ public class Parser {
 
     }
     public void checkUserInput(String userInput, ModuleList listOfModules) {
-        String userCommand = userInput.split("/")[0];
+        String userCommand = userInput.split("/")[0].trim();
         switch (userCommand) {
         case "man":
             Manpage.printManPage();
+            break;
+        case "add":
+            listOfModules.addModule(userInput.split("/")[1].trim(), userInput.split("/")[2]);
             break;
         case "find":
             listOfModules.findModule(userInput.split("/")[1]);
