7f111dea8eef46891b1d08c34662b64ca979fd29
saint thiri zaw <saintstz@gmail.com>
2023-03-09 10:24:23 +0800

    Fixed indentation in Parser class

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,29 +1,30 @@
 package seedu.duke;
 
 public class Parser {
-    public Parser () {
+    public Parser() {
 
     }
+
     public void checkUserInput(String userInput, ModuleList listOfModules) {
         String userCommand = userInput.split("/")[0].trim();
         switch (userCommand) {
-            case "man":
-                Manpage.printManPage();
-                break;
-            case "add":
-                listOfModules.addModule(userInput.split("/")[1].trim(), userInput.split("/")[2]);
-                break;
-            case "find":
-                listOfModules.findModule(userInput.split("/")[1]);
-                break;
-            case "delete":
-                listOfModules.deleteModule(userInput.split("/")[1]);
-                break;
-            case "view":
-                listOfModules.viewModuleList();
-                break;
-            default:
-                break;
+        case "man":
+            Manpage.printManPage();
+            break;
+        case "add":
+            listOfModules.addModule(userInput.split("/")[1].trim(), userInput.split("/")[2]);
+            break;
+        case "find":
+            listOfModules.findModule(userInput.split("/")[1]);
+            break;
+        case "delete":
+            listOfModules.deleteModule(userInput.split("/")[1]);
+            break;
+        case "view":
+            listOfModules.viewModuleList();
+            break;
+        default:
+            break;
         }
 
     }
