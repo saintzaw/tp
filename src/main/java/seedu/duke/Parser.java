17f9cc42b74e1c8063071fcd261109f85fba5377
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-03-14 20:05:21 +0800

    Added Exceptions, Custom Exception Class and Logs for addModule inputs

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -15,7 +15,6 @@ public class Parser {
         String userCommand = userInput.split("/")[1].trim();
 
         switch (userCommand) {
-
         case "add":
             Manpage.printManPageAdd();
             break;
@@ -48,9 +47,12 @@ public class Parser {
             }
             break;
         case "add":
-            assert userCommands.length == 4;
-            listOfModules.addModule(userCommands[1].trim(),
-                    userCommands[2].trim(), userCommands[3].trim());
+            try {
+                checkAddInput(listOfModules, userCommands);
+            } catch (DukeException e) {
+                LOGGER.log(Level.WARNING, "addModule Check failed: " + e.getDescription());
+                Print.printErrorMessage(e);
+            }
             break;
         case "find":
             assert userCommands.length == 2;
@@ -73,7 +75,40 @@ public class Parser {
         default:
             break;
         }
+    }
 
+    private void checkAddInput(ModuleList listOfModules, String[] userCommands) throws DukeException {
+        //check for correct number of fields
+        if (userCommands.length < 4) {
+            DukeException inputException = new DukeException("Missing fields");
+            throw inputException;
+        } else if (userCommands.length > 4) {
+            DukeException inputException = new DukeException("Too many fields");
+            throw inputException;
+        }
+        //check for correct field in MC
+        try {
+            int moduleCredits = Integer.parseInt(userCommands[2].trim());
+            if (moduleCredits < 0 || moduleCredits > 8) {
+                DukeException inputException = new DukeException("Make sure Modular Credits is a number from 0 to 8");
+                throw inputException;
+            }
+        } catch (NumberFormatException e) {
+            DukeException inputException = new DukeException("Make sure Modular Credits is a number from 0 to 8");
+            throw inputException;
+        }
+        //check for correct field in type of module
+        String typeOfModule = userCommands[3].trim();
+        boolean isCorrectModuleType = typeOfModule.equals("CORE") || typeOfModule.equals("UE")
+                || typeOfModule.equals("GE");
+        if (!isCorrectModuleType) {
+            DukeException inputException = new DukeException("Incorrect Module Type, "
+                    + "Accepted Module Types are: (CORE,UE,GE)");
+            throw inputException;
+        }
+        assert userCommands.length == 4;
+        listOfModules.addModule(userCommands[1].trim(),
+                userCommands[2].trim(), userCommands[3].trim());
     }
 
 }
