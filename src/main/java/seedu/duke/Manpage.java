94579bd8982c97d0821208e8eef73d8764f79844
anthea-pr0g <antheayip02@gmail.com>
2023-03-15 23:15:27 +0800

    anthea- fix manpage ui string static

--- src/main/java/seedu/duke/Manpage.java
+++ src/main/java/seedu/duke/Manpage.java
@@ -1,7 +1,7 @@
 
 package seedu.duke;
 public class Manpage {
-    private final static String line= "-----------------------------------------------------\n";
+
     public Manpage() {
 
     }
@@ -34,12 +34,12 @@ public class Manpage {
                 "Exit\n " +
                 "Command: bye\n "+
                 "To find out more, use the command: man /bye\n " +
-                line );
+                 "-----------------------------------------------------\n" );
     }
 
     public static void printManPageAdd() {
         System.out.println("\n\n" +
-                line+
+                 "-----------------------------------------------------\n"+
                 "Adding a module: add\n Adds a module to the list of modules.\n " +
                 "Format: add /MODULE_CODE /MODULAR_CREDITS /MODULE_TYPE\n " +
                 "Example: add /CS2113T /4 /CORE\n\n " +
@@ -48,10 +48,10 @@ public class Manpage {
                 "CORE\n " +
                 "GE (General modules)\n " +
                 "UE (Unrestricted Electives)\n\n" +
-                line);
+                "-----------------------------------------------------\n");
     }
     public static void printManPageList() {
-        System.out.println("\n\n" + line +
+        System.out.println("\n\n" + "-----------------------------------------------------\n" +
 
                 "List all modules: list all \n " +
                 "Shows a list of all modules in the module planner along with the module type.\n " +
@@ -60,34 +60,34 @@ public class Manpage {
                 "List all modules: list /(Year) \n " +
                 "Shows a list of all modules in that year along with the module type.\n " +
                 "Example: list 1\n\n" +
-                 line);
+                "-----------------------------------------------------\n");
     }
 
     public static void printManPageFind() {
         System.out.println("\n\n" +
-                line+
+                "-----------------------------------------------------\n"+
 
                 "Find module: find /(Module Code)\n " +
                 "Find which year and semester I plan to take the module.\n " +
                 "Example: find /CS2113T\n\n "
-                + line);
+                + "-----------------------------------------------------\n");
     }
 
     public static void printManPageDelete() {
         System.out.println("\n\n" +
-                line+
+                "-----------------------------------------------------\n"+
                 "Delete module: delete /(Module Code)\n " +
                 "Shows a list of all modules in the module planner.\n " +
-                "Example: delete /CS2113T\n\n " + line );
+                "Example: delete /CS2113T\n\n " + "-----------------------------------------------------\n" );
     }
 
     public static void printManPageExit() {
         System.out.println("\n\n" +
-                line+
+                "-----------------------------------------------------\n"+
                 "Exit program: bye\n " +
                 "Exit module planner.\n " +
                 "Format: bye\n\n"
-                +line);
+                +"-----------------------------------------------------\n");
     }
 
 }
