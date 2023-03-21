5bb1bbdad2aaa10ad44923f5b7c5cbb1c93b5b1d
LAPTOP-OFQC49LA\gk96 <geraldkoh4@gmail.com>
2023-03-21 11:11:15 +0800

    More checkstyle changes

--- src/test/java/seedu/duke/ModuleListTest.java
+++ src/test/java/seedu/duke/ModuleListTest.java
@@ -1,8 +1,8 @@
 package seedu.duke;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
 class ModuleListTest {
     /*
