41f021cf8c07da2beb38ea299378bb115e805d24
Goh Chok Yao <chokyao@gmail.com>
2023-04-03 23:15:12 +0800

    Fix bug

--- src/main/java/seedu/main/Print.java
+++ src/main/java/seedu/main/Print.java
@@ -9,9 +9,9 @@ public class Print {
     public static void printHelloMessage(String name) {
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello " + name + "!");
-        System.out.println("     I'm your personal Modganiser.                              ");
-        System.out.println("     What would you like to plan today?                         ");
-        System.out.println("     To find out more, type `man` and press enter :)            ");
+        System.out.println("     I'm your personal Modganiser.");
+        System.out.println("     What would you like to plan today?");
+        System.out.println("     To find out more, type `man` and press enter :)");
         System.out.println("    ____________________________________________________________");
     }
 
